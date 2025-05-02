a60fbd86af9f33124a7fca73cf05c2ae23ac581a
katlegoprince1998 <princekatlego357@gmail.com>
2025-05-02 18:30:45 +0200

    Project setup

---
+++ src/main/java/com/qda/reading_tracker/ReadingTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.qda.reading_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadingTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadingTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qda/reading_tracker/ReadingTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qda.reading_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadingTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
